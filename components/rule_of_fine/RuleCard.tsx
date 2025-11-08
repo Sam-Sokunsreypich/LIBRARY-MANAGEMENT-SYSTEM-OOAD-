6d0e286166187588330eefaf86991d2abb6dd5c6
Sam-Sokunsreypich <sokunsreypichsam@gmail.com>
2025-11-08 23:01:09 +0700

    modify: rule fine

--- components/rule_of_fine/RuleCard.tsx
+++ components/rule_of_fine/RuleCard.tsx
@@ -3,14 +3,32 @@
 import { Pen, Trash } from 'lucide-react'
 import { useState } from 'react'
 import EditFine from './EditFine'
+import { RuleType } from '@/types/RuleType'
+import { DeleteFine } from './DeleteFine'
+import { deleteFine } from '@/app/admin/rule_of_fine/action/rule'
+import { toast } from 'sonner'
 
 interface RuleCardProps {
   ruleData: RuleType
-  onDelete?: () => void 
 }
 
-export default function RuleCard({ ruleData, onDelete }: RuleCardProps) {
+export default function RuleCard({ ruleData }: RuleCardProps) {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false)
+
+  async function onDelete() {
+        try{
+          if (!ruleData?.id) {
+            toast.error("Missing rule ID!");
+            return;
+          }
+          await deleteFine(ruleData.id);
+          toast.success("Rule Deleted successfully!");
+        }catch (error) {
+          console.error(error);
+          toast.error("Failed to update rule");
+        }
+        
+    }
 
   return (
     <div className='relative border border-gray-300 rounded-xl p-4 max-w-96 max-h-80'>
@@ -33,10 +51,12 @@ export default function RuleCard({ ruleData, onDelete }: RuleCardProps) {
             <Pen className='text-yellow-400 w-4 h-4'/>
           </button>
           <button
-            onClick={onDelete}
             aria-label="Delete"
           >
-            <Trash className='text-red-500 w-4 h-4'/>
+            {/* <Trash className='text-red-500 w-4 h-4'/> */}
+            <DeleteFine 
+            onDelete={onDelete}
+            />
           </button>
         </div>
       </div>
